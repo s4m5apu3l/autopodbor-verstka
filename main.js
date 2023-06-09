f164e8dc516a1eb19b452db153b7409a040c47fe
s4m5apu3l <asperatuz@yandex.ru>
2023-06-09 23:45:08 +0600

    feat: pochti done work 90%

---
+++ main.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper(".js-swiper", {
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  spaceBetween: 20,
+});
