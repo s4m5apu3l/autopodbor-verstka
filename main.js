1955d422693089e0b7a807c2ffb0ec9df5bf2941
s4m5apu3l <asperatuz@yandex.ru>
2023-06-10 20:35:38 +0600

    feat: add modal

--- main.js
+++ main.js
@@ -8,3 +8,17 @@ const swiper = new Swiper(".js-swiper", {
   },
   spaceBetween: 20,
 });
+
+document.querySelectorAll('.js-open__slider-modal').forEach((btn) => {
+  btn.addEventListener('click', function() {
+    const modal = this.nextElementSibling;
+    modal.classList.add('show');
+  })
+})
+
+document.querySelectorAll('.js-open__slider-modal').forEach((btn) => {
+  btn.addEventListener('click', function() {
+    const modal = this.nextElementSibling;
+    modal.classList.add('show');
+  })
+})
